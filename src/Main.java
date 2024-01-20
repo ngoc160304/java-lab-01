6d86d4ca2c280620eaa14b49cea3bee33c2486d0
ngoc160304 <ngocdev160304@gmail.com>
2024-01-20 09:32:06 +0700

    them bai tap 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+//    Bai Tap 1
     public static boolean isPrime (long n) {
         for(int i = 2; i <= Math.sqrt(n); i++) {
             if(n % i == 0) {
@@ -9,6 +13,7 @@ public class Main {
         }
         return n > 1;
     }
+//    Bai Tap 1
     public static boolean isFibo(long n) {
         if(n == 0 || n == 1) {
             return true;
@@ -24,6 +29,7 @@ public class Main {
         }
         return false;
     }
+//    Bai Tap 2
     public static String convertString(String s) {
         String token[] = s.trim().split("\\s+");
         String result = "";
@@ -32,8 +38,25 @@ public class Main {
         }
         return result.trim();
     }
+//    Bai Tap 3
+    public static int countCharacter(String s1, String s2) {
+        int cnt = 0;
+        Set<Character> se = new HashSet<Character>();
+        for(int i = 0; i < s1.length(); i++) {
+            se.add(s1.charAt(i));
+        }
+
+        for(int i = 0; i < s2.length(); i++) {
+            if(se.contains(s2.charAt(i))) {
+                ++cnt;
+                se.remove(s2.charAt(i));
+            }
+        }
+        return cnt;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+//        Bai Tap 1
         long n = sc.nextLong();
         if(isFibo(n) && isPrime(n)) {
             System.out.println("So hoan hao");
@@ -42,7 +65,11 @@ public class Main {
             System.out.println("So khong hoan hao");
         }
         sc.nextLine();
+//        Bai Tap 2
         String s = sc.nextLine();
         System.out.println(convertString(s));
+//     Bai Tap 3
+        String s1 = sc.nextLine(), s2 = sc.nextLine();
+        System.out.println(countCharacter(s1, s2));
     }
 }
