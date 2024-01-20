3da9b570149298abfa3b19a1c4bf564d660b3d62
ngoc160304 <ngocdev160304@gmail.com>
2024-01-20 08:46:37 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isPrime (long n) {
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return n > 1;
+    }
+    public static boolean isFibo(long n) {
+        if(n == 0 || n == 1) {
+            return true;
+        }
+        long f0 = 0, f1 = 1;
+        for(int i = 2; i <= 90; i++) {
+            long f = f0 + f1;
+            if(f == n) {
+                return true;
+            }
+            f0 = f1;
+            f1 = f;
+        }
+        return false;
+    }
+    public static String convertString(String s) {
+        String token[] = s.trim().split("\\s+");
+        String result = "";
+        for(int i = 0; i < token.length; i++) {
+            result += token[i] + " ";
+        }
+        return result.trim();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
